7db912f84b57d2d5e4fc4cfc1bd116063f0b613a
Nabeel Zubair <nabeel@devigital.com>
2021-02-03 12:13:37 +0500

    Added delay in test action

--- src/store/sagas/test.sagas.ts
+++ src/store/sagas/test.sagas.ts
@@ -1,11 +1,13 @@
-import {put, takeEvery, all} from 'redux-saga/effects';
+import {put, takeEvery, all, delay} from 'redux-saga/effects';
 import {testActions} from '../actions/';
 import {FILTER_ACTIONS} from '../actions/reduxActions';
 
 function* testSaga() {
   try {
     yield put(testActions.testAction.pending);
+    yield delay(500);
     yield put(testActions.testAction.success('succeeded!'));
+    yield delay(500);
   } catch {
     yield put(testActions.testAction.error('failed!'));
   }
